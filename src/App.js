541ac589956a7fc6585daa2b55bf7fd5d2489326
olygood <olivier.huttmacher@gmail.com>
2021-05-20 15:00:47 +0200

    site web on react babel and  webpack on scratch, img and css

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+//import css 
+import "app.css";
+//import images
+import toad from './img/toad.png';
+
+const App = ()=> (
+    <div>
+        <h1 className="colorText">Bien venu sur le site ohstyle</h1>
+        <img src="./src/img/toad.png"></img>
+        
+    </div>
+);
+
+export default App;
